82debb117fa5caac00021c4124fc2064d68b057a
Otávio Guerra <otaviomguerra@hotmail.com>
2022-06-22 22:21:03 -0300

    feat: Added write_to_csv function to write the csv formated user to stdout

--- json_to_csv/src/main.rs
+++ json_to_csv/src/main.rs
@@ -1,7 +1,8 @@
-use serde::Deserialize;
+use serde::{Serialize, Deserialize};
 use reqwest::{Error, blocking::Client};
+use std::{io};
 
-#[derive(Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug)]
 #[serde(rename_all = "camelCase")]
 struct User {
     user_id: i32,
@@ -10,19 +11,39 @@ struct User {
     body: String,
 }
 
-fn get_json_api_data() -> Result<(), Error> {
+fn clean_api_text_content(text: &str) -> String {
+    text.replace("\n", "")
+}
+
+fn get_json_api_data() -> Result<User, Error> {
     let client = Client::new();
-    let resp = client.get("https://jsonplaceholder.typicode.com/posts/3").send()?;
-    let user: User = resp.json()?;
+    let resp = client.get("https://jsonplaceholder.typicode.com/posts/4").send()?;
+    let mut user: User = resp.json()?;
 
-    println!("User: {:#?}", user);
+    user.body = clean_api_text_content(&user.body);
+    user.title = clean_api_text_content(&user.title);
 
+    Ok(user)
+}
+
+fn write_to_csv(user: User) -> Result<(), csv::Error> {
+    let mut wtr = csv::Writer::from_writer(io::stdout());
+    wtr.serialize(user)?;
+    wtr.flush()?;
+    
     Ok(())
 }
 
 
 fn main() {
-    if let Err(err) = get_json_api_data() {
-        eprintln!("Something went wrong, {}", err);
-    }
+
+    let json_data = match get_json_api_data() {
+        Ok(user) => user,
+        Err(err) => panic!("Something went wrong upon getting the json data, {}", err),
+
+    };
+    println!("User got from API:\n {:#?}", json_data);
+    println!("\nCSV line generated:\n");
+    _ = write_to_csv(json_data);
+
 }
